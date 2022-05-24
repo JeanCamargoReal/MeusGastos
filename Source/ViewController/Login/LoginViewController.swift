bb002f18a2f27014962832fa70d4263426e63bcf
JeanCamargoReal <jeancamargoreal@icloud.com>
2022-05-24 15:18:44 -0300

    Cria LoginView

--- Source/ViewController/Login/LoginViewController.swift
+++ Source/ViewController/Login/LoginViewController.swift
@@ -5,4 +5,18 @@
 //  Created by Jean Camargo on 23/05/22.
 //
 
-import Foundation
+import UIKit
+
+class LoginViewController: UIViewController {
+	
+	lazy var loginView: LoginView = {
+		let view = LoginView()
+		view.backgroundColor = .white
+		
+		return view
+	}()
+	
+	override func loadView() {
+		self.view = loginView
+	}
+}
